19e036a3d1c0a55ca0caf91d6a2b35e18b3d914a
Ana Paula dos Reis <anareis87@icloud.com>
2025-09-03 21:26:46 -0300

    03 de SETEMBRO

--- OT_01/condicionais.php
+++ OT_01/condicionais.php
@@ -1,5 +1,15 @@
 <?php
 
-    
+$numero = -5; //altere aqui para testar o código!
 
+echo "Número: $numero\n";
+
+
+if ($numero == 0) {
+    echo "<p>O número é zero (par).</p>";
+} else {
+    $sinal = ($numero > 0) ? "positivo" : "negativo";
+    $paridade = ($numero % 2 == 0) ? "par" : "ímpar";
+    echo "<p>O número é $sinal e $paridade.</p>";
+}
 ?>
